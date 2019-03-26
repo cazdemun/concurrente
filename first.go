b37258625e22275bd6f8ff8a5421adb131d384ef
wilfredo morales <wmorales83@gmail.com>
2019-03-26 11:03:31 -0500

    as

--- first.go
+++ first.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-var n int = 0;
+var n int = 0
+
+func P() {
+	var temp int
+	for i := 0; i < 10; i++ {
+		temp = n
+		n = temp + 1
+	}
+}
+
+func Q() {
+	var temp int
+	for i := 0; i < 10; i++ {
+		temp = n
+		n = temp + 1
+	}
+}
 
 func main() {
-    fmt.Println("hello world")
+	go P()
+	go Q()
+	time.Sleep(100*time.Millisecond)
+    fmt.Println(n)
 }
 
